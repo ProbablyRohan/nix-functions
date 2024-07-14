80ed458606d130709f9d06ba864cd42f4b0e5bac
ProbablyRohan <rmg1@mail.com>
2024-07-14 20:56:11 +1000

    Adds writeNushellScript function

---
+++ lib.nix
@@ -0,0 +1,14 @@
+{ pkgs, ... }: {
+  writeNushellScript = pkgs.lib.makeOverridable ({ pname, version, src, installPath, ... }:
+    pkgs.stdenvNoCC.mkDerivation {
+      inherit pname version src
+      dontUnpack = true;
+      installPhase = ''
+        mkdir $out/bin -p
+        echo "#!${pkgs.nushell}/bin/nu" > $out${installPath}
+        cat $src |tail -n+2>> $out${installPath}
+        chmod +x $out${installPath}
+      ''; 
+    }
+  );
+}
